b8709a7dffd7058bd6b60d1143ab47bed6339b51
sarah garcin <sarah@g-u-i.me>
2015-03-25 00:10:03 +0100

    add png images for menu

--- cache/compiled/files/1ca7e8705150988855f814533d46b1eb.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'user/plugins/template/template.yaml',
-    'modified' => 1427213422,
-    'data' => [
-        'enabled' => true
-    ]
-];
--- user/themes/usg-mobile/js/script.js
+++ user/themes/usg-mobile/js/script.js
@@ -3,9 +3,13 @@ $(document).ready(function(){
 	$(".menu-toggle").on('click', function(){
 		$('.navbar').slideToggle({duration:1000});
 	});
-	$(".content").on('click', function(){
+	$(".content img").on('click', function(){
 		$('.navbar').slideToggle({duration:1000});
 	});
 
+	$(".sommaire li").on('click', function(){
+		$(".sommaire li.Expositions>a").removeAttr('href');
+		$(this).children('ul').slideToggle({duration:1000});
+	});
 
 });
