5c196cc6c9190ec2b59ae19a8e0e64c520f302b0
sarah garcin <sarah@g-u-i.me>
2015-03-25 18:43:28 +0100

    clean

--- cache/compiled/files/78e16ec3bda07470c26a6f008a5961d5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://usg-mobile/usg-mobile.yaml',
-    'modified' => 1425289281,
-    'data' => [
-        'enabled' => true
-    ]
-];
